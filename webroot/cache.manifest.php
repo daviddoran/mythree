6199a933aeb06320a074f7fa59d1c3c3a4dec6f2
David Doran <daviddoranmedia@gmail.com>
2012-10-24 22:23:21 +0100

    Add cache manifest for fast app updating (and partial offline functionality).

---
+++ webroot/cache.manifest.php
@@ -0,0 +1,22 @@
+<?php
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Content-type: text/cache-manifest");
+?>
+CACHE MANIFEST
+#rev001
+
+NETWORK:
+/api.php
+
+CACHE:
+/jquery-1.8.2.min.js
+/office-001.png
+/touch-icon-ipad-72.png
+/touch-icon-ipad-144.png
+/touch-icon-iphone-57.png
+/touch-icon-iphone-114.png
+/add2home/add2home.css
+/add2home/add2home.js
