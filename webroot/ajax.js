ab7f8c2e2d86a17d8af97d4ea7ccd7b548965082
David Doran <daviddoranmedia@gmail.com>
2013-02-02 01:53:06 +0000

    Minify ajax.js.

--- webroot/ajax.js
+++ webroot/ajax.js
@@ -2,18 +2,19 @@
  * Based on code from http://www.quirksmode.org/js/xmlhttp.html
  */
 (function () {
+    "use strict";
     var XMLHttpFactories = [
         function () {
-            return new XMLHttpRequest()
+            return new XMLHttpRequest();
         },
         function () {
-            return new ActiveXObject("Msxml2.XMLHTTP")
+            return new ActiveXObject("Msxml2.XMLHTTP");
         },
         function () {
-            return new ActiveXObject("Msxml3.XMLHTTP")
+            return new ActiveXObject("Msxml3.XMLHTTP");
         },
         function () {
-            return new ActiveXObject("Microsoft.XMLHTTP")
+            return new ActiveXObject("Microsoft.XMLHTTP");
         }
     ];
 
@@ -33,7 +34,7 @@
     var quirksmode = {};
     quirksmode.sendRequest = function (url, callback, postData) {
         var req = createXMLHTTPObject();
-        if (!req) return;
+        if (!req){ return; }
         var method = (postData) ? "POST" : "GET";
         req.open(method, url, true);
         req.setRequestHeader("X-Requested-With", "XMLHttpRequest");
@@ -41,13 +42,13 @@
             req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         }
         req.onreadystatechange = function () {
-            if (req.readyState != 4) return;
-            if (req.status != 200 && req.status != 304) {
+            if (req.readyState !== 4) { return; }
+            if (req.status !== 200 && req.status !== 304) {
                 return;
             }
             callback(req);
         };
-        if (req.readyState == 4) return;
+        if (req.readyState === 4) { return; }
         req.send(postData);
     };
     window.quirksmode = quirksmode;
